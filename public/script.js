29a7f8c52a02b79c4e9cc3986243de14a106c0df
Jieung Park <parkje0623@gmail.com>
2020-06-01 23:05:38 -0700

    Tutorial watched

---
+++ public/script.js
@@ -0,0 +1,11 @@
+// function asc(a,b){
+//   return a-b;
+// }
+
+//For each value in the grid (solution)
+//Do a <td class="solution or none"> to define its cell
+
+//Make something happen when clicked
+var a = document.getElementById("Reset");
+
+//Watch tutorial again for details
