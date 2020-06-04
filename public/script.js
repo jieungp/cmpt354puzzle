ae8589b58a17ab92d49ea556f567a43b084fccc6
Jieung Park <parkje0623@gmail.com>
2020-06-03 17:28:30 -0700

    Restart fixed, need to fix hover

--- public/script.js
+++ public/script.js
@@ -1,9 +1,39 @@
-//Make something happen when clicked
-var t = document.getElementsByTagName("td");
+var cells = document.getElementsByTagName("td");
+var purpleLeft = 0;
+restartGame();
 
 function restartGame() {
-  //document.getElementsById("wholetable").style.backgroundColor="red";
-  for (var i = 0; i < t.length; i++) {
-    t[i].style.backgroundColor="turquoise";
+  for (var i = 0; i < cells.length; i++) {
+    cells[i].style.backgroundColor="turquoise";
+  }
+}
+
+function giveSolution() {
+  for (var j = 0; j < cells.length; j++) {
+    cells[j].click();
+  }
+  alertMsg();
+}
+
+function alertMsg() {
+  alert("CONGRATULATION, You have Won!");
+}
+
+function finished() {
+  for (var k = 0; k < cells.length; k++) {
+    if (cells[k].style.backgroundColor === "turquoise") {
+      cells[k].click();
+      if (cells[k].style.backgroundColor !== "white") {
+        purpleLeft = 1;
+      }
+      cells[k].style.backgroundColor="turquoise";
+    }
+  }
+
+  if (purpleLeft) {
+    alert("FAIL, You have not finished yet!");
+    purpleLeft = 0;
+  } else {
+    giveSolution();
   }
 }
